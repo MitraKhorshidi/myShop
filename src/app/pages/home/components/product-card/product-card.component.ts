f4bc440314baad5050fcfe8775aeef865a9bf5c6
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-10-13 14:59:50 +0200

    define addToCart option

--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component , Input } from '@angular/core';
+import { Component , Input , Output , EventEmitter } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-card',
@@ -8,5 +9,19 @@ import { Component , Input } from '@angular/core';
 export class ProductCardComponent {
 
   @Input() fullWidthMode : boolean = false;
+  @Output() addtoCart = new EventEmitter<Product>;
+  product :Product ={
+    id:1,
+    title:'Skeatures',
+    price: 150,
+    category: 'shoes',
+    description:'Some desc for the product',
+    image:"https://placehold.co/150",
+
+  }
+
+  onAddToCart():void{
+    this.addtoCart.emit(this.product);
+  }
 
 }
